3fab8c0890ea46f6c3a6fe8ee278f8c962c55f61
Babak Jameei <babak.jameei@outlook.com>
2024-04-26 20:24:44 +0200

    JSON data sorteras korrekt efter typ

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,48 +1,52 @@
-// Load the courses.json file using ES6 syntax
+// Hämta JSON data med kurser
 fetch('https://studenter.miun.se/~mallar/dt211g/')
   .then(response => response.json())
   .then(data => {
-    createChart(data);
+    createBarChart(data);
+    createPieChart(data);
   });
-  data.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
 
+function createBarChart(data) {
 
+  // Sortera data
+  const courses = data
+  .filter(item => item.type === 'Kurs')
+  .sort((a, b) => b.applicantsTotal - a.applicantsTotal);
 
-  function createChart(data) {
-    // Hämtar namn på de 6 mest sökta kurserna
-    const labels = data.slice(0, 6).map(course => course.name); 
-    // Hämtar antal sökande för de 6 mest sökta kurserna
-    const values = data.slice(0, 6).map(course => course.applicantsTotal); 
-  
-    const chartContainer = document.getElementById('chart-container');
-    const chart = new Chart(chartContainer, {
-        // Typ av diagram (stapeldiagram)
-      type: 'bar', 
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            label: 'Totalt antal sökande',
-            data: values,
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#99FF99', '#C74C70'], // Färger för staplarna
-            borderColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#99FF99', '#C74C70'], // Ramfärger för staplarna
-          }
-        ]
-      },
-      options: {
-        responsive: true,
-        title: {
-          display: true,
-          text: 'De 6 mest sökta kurserna på Mittuniversitetet, HT23'
-        },
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true 
-            }
-          }]
+  // Hämtar namn på de 6 mest sökta kurserna
+  const courseLabels = courses.slice(0, 6).map(course => course.name);
+  // Hämtar antal sökande för de 6 mest sökta kurserna
+  const courseValues = courses.slice(0, 6).map(course => course.applicantsTotal);
+
+  const chartContainer = document.getElementById('bar-chart-container');
+  new Chart(chartContainer, {
+    // Typ av diagram (stapeldiagram)
+    type: 'bar',
+    data: {
+      labels: courseLabels,
+      datasets: [
+        {
+          label: 'Totalt antal sökande',
+          data: courseValues,
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#99FF99', '#C74C70'], // Färger för staplarna
+          borderColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#99FF99', '#C74C70'], // Ramfärger för staplarna
         }
+      ]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'De 6 mest sökta kurserna på Mittuniversitetet, HT23'
+      },
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
       }
-    });
-  }
-  
+    }
+  });
+}
+
